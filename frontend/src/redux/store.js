dce227e538110072bb3532de2d9338899780e9f9
Rahul Jangra <rjangra4015@gmail.com>
2025-07-03 15:54:54 +0530

    spotify api

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -2,18 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import playerReducer from "./features/playerSlice";
 
-import { shazamCoreApi } from "./services/shazamCore";
-
 import { spotifyApi } from "./services/spotify";
 
 export const store = configureStore({
   reducer: {
-    // [shazamCoreApi.reducerPath]: shazamCoreApi.reducer,
     [spotifyApi.reducerPath]: spotifyApi.reducer,
     player: playerReducer,
   },
-  // middleware: (getDefaultMiddleware) =>
-  //   getDefaultMiddleware().concat(shazamCoreApi.middleware),
-    middleware: (getDefaultMiddleware) =>
+  middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(spotifyApi.middleware),
 });
