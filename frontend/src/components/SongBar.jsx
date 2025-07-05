76a2f663596419dbb48980c87b0eb54311ae8498
Rahul Jangra <rjangra4015@gmail.com>
2025-07-05 11:55:51 +0530

    added top bar

--- frontend/src/components/SongBar.jsx
+++ frontend/src/components/SongBar.jsx
@@ -20,11 +20,7 @@ const SongBar = ({
     <div className="flex-1 flex flex-row justify-between items-center">
       <img
         className="w-20 h-20 rounded-lg"
-        src={
-          artistId
-            ? song?.images?.[0]?.url // For artist images, if applicable
-            : song?.album?.images?.[0]?.url // For track album cover
-        }
+        src={song?.album?.images?.[0]?.url}
         alt={song?.name}
       />
       <div className="flex-1 flex flex-col justify-center mx-3">
@@ -35,15 +31,12 @@ const SongBar = ({
             </p>
           </Link>
         ) : (
-          <p className="text-xl font-bold text-white">
-            {song?.name} {/* Artist name if applicable */}
-          </p>
+          <p className="text-xl font-bold text-white">{song?.name}</p>
         )}
         <p className="text-base text-gray-300 mt-1">
           {artistId
-            ? song?.genres?.[0] || "Artist" // Genre fallback
-            : song?.album?.name}{" "}
-          // Album name
+            ? song?.genres?.[0] || "Genre not available"
+            : song?.artists?.[0]?.name || "Unknown Artist"}
         </p>
       </div>
     </div>
