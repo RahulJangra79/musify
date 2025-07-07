05f68e55dc26f2caa4484b4b6915a7ab0e06b129
Rahul Jangra <rjangra4015@gmail.com>
2025-07-07 16:35:40 +0530

    added around you

--- frontend/src/pages/AroundYou.jsx
+++ frontend/src/pages/AroundYou.jsx
@@ -1,5 +1,37 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
-const CountryTracks = () => <div>CountryTracks</div>;
+import { Error, Loader, SongCard } from '../components';
+import { useGetTopTracksQuery } from '../redux/services/spotify';
+
+const CountryTracks = () => {
+  const country = 'IN';
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
+  const { data, isFetching, error } = useGetTopTracksQuery();
+
+  if (isFetching) return <Loader title={`Loading Songs around ${country}...`} />;
+  if (error) return <Error />;
+
+  return (
+    <div className="flex flex-col">
+      <h2 className="font-bold text-3xl text-white text-left mt-4 mb-10">
+        Around you <span className="font-black">{country}</span>
+      </h2>
+
+      <div className="flex flex-wrap sm:justify-start justify-center gap-8">
+        {data?.items?.map((song, i) => (
+          <SongCard
+            key={song.id}
+            song={song}
+            isPlaying={isPlaying}
+            activeSong={activeSong}
+            data={data.items}
+            i={i}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default CountryTracks;
