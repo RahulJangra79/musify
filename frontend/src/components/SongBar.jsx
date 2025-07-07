bc3d634f4f424d3ad6f68f6e8b1475aa2ecfa70c
Rahul Jangra <rjangra4015@gmail.com>
2025-07-07 18:58:50 +0530

    artists song name click

--- frontend/src/components/SongBar.jsx
+++ frontend/src/components/SongBar.jsx
@@ -22,11 +22,18 @@ const SongBar = ({
         alt={song?.name}
       />
       <div className="flex-1 flex flex-col justify-center mx-3">
-        {!artistId ? (
+        {/* {!artistId ? (
           <Link to={`/songs/${song?.id}`}>
             <p className="text-xl font-bold text-white">
               {song?.name}
             </p>
+          </Link>
+        ) : (
+          <p className="text-xl font-bold text-white">{song?.name}</p>
+        )} */}
+        {!artistId ? (
+          <Link to={`/songs/${song?.id}`} onClick={(e) => e.stopPropagation()}>
+            <p className="text-xl font-bold text-white">{song?.name}</p>
           </Link>
         ) : (
           <p className="text-xl font-bold text-white">{song?.name}</p>
