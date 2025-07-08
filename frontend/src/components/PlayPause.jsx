7d4ee0a97c6b0ecdcf2bd8bb8de82774d70135ed
Rahul Jangra <rjangra4015@gmail.com>
2025-07-08 13:41:42 +0530

    adding play pause func

--- frontend/src/components/PlayPause.jsx
+++ frontend/src/components/PlayPause.jsx
@@ -2,16 +2,20 @@ import { FaPauseCircle, FaPlayCircle } from "react-icons/fa";
 
 const PlayPause = ({ isPlaying, activeSong, song, handlePause, handlePlay }) =>
   isPlaying && activeSong?.id === song.id ? (
-    <FaPauseCircle
-      size={35}
-      className="text-gray-300"
-      onClick={handlePause}
-    />
+    <FaPauseCircle size={35} className="text-gray-300" onClick={handlePause} />
   ) : (
+    // <FaPlayCircle
+    //   size={35}
+    //   className="text-gray-300"
+    //   onClick={handlePlay}
+    // />
     <FaPlayCircle
       size={35}
       className="text-gray-300"
-      onClick={handlePlay}
+      onClick={(e) => {
+        e.stopPropagation();
+        handlePlay();
+      }}
     />
   );
 
--- frontend/src/components/SongCard.jsx
+++ frontend/src/components/SongCard.jsx
@@ -34,7 +34,7 @@ const SongCard = ({ song, isPlaying, activeSong, i, data }) => {
   const artistId = song?.artists?.[0]?.id;
 
   return (
-    <div className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer">
+    <div className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer" onClick={handlePlayClick}>
       <div className="relative w-full h-56 group">
         <div
           className={`absolute inset-0 justify-center items-center bg-black bg-opacity-50 group-hover:flex ${
