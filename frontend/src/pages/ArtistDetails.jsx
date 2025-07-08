ccccf2ae1d7b05a24620e5560714e577f65e89dd
Rahul Jangra <rjangra4015@gmail.com>
2025-07-08 15:43:04 +0530

    changes in artist

--- frontend/src/pages/ArtistDetails.jsx
+++ frontend/src/pages/ArtistDetails.jsx
@@ -1,5 +1,5 @@
 import { useParams } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { DetailsHeader, Error, Loader, RelatedSongs } from "../components";
 import {
   useGetArtistDetailsQuery,
