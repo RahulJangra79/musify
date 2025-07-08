dda7bf838b63fefec4ba233969864d86979dbb56
Rahul Jangra <rjangra4015@gmail.com>
2025-07-08 11:06:03 +0530

    remove app.css

--- frontend/src/components/TopPlay.jsx
+++ frontend/src/components/TopPlay.jsx
@@ -36,7 +36,7 @@ const TopChartCard = ({
       />
       <div className="flex-1 flex flex-col justify-center mx-3">
         <Link to={`/songs/${song.id}`}>
-          <p className="text-xl font-bold text-white">{song?.name}</p>
+          <p className="text-lg font-bold text-white">{song?.name}</p>
         </Link>
         <Link to={`/artists/${song?.artists[0]?.id}`}>
           <p className="text-base text-gray-300 mt-1">
