744bbd1b44ad7449e1f9f97ae8ad1cb250452035
Rahul Jangra <rjangra4015@gmail.com>
2025-07-13 11:01:26 +0530

    changed style

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -29,7 +29,7 @@ const App = () => {
   return (
     <div className="relative flex">
       <Sidebar />
-      <div className="flex-1 flex flex-col bg-gradient-to-br from-black to-[#121286]">
+      <div className="flex-1 flex flex-col bg-gradient-to-br from-black to-[#1a1a1a]">
         <Searchbar />
 
         <div className="px-6 h-[calc(100vh-72px)] overflow-y-scroll hide-scrollbar flex xl:flex-row flex-col-reverse">
--- frontend/src/components/SongBar.jsx
+++ frontend/src/components/SongBar.jsx
@@ -1,53 +1,3 @@
-// import { Link } from "react-router-dom";
-// import PlayPause from "./PlayPause";
-
-// const SongBar = ({
-//   song,
-//   i,
-//   artistId,
-//   isPlaying,
-//   activeSong,
-//   handlePauseClick,
-//   handlePlayClick,
-// }) => (
-//   <div
-//     className={`w-full flex flex-row items-center hover:bg-[#4c426e] ${
-//       activeSong?.name === song?.name ? "bg-[#4c426e]" : "bg-transparent"
-//     } py-2 p-4 rounded-lg cursor-pointer mb-2`}
-//   >
-//     <div className="flex-1 flex flex-row justify-between items-center">
-//       <img
-//         className="w-20 h-20 rounded-lg"
-//         src={song?.album?.images?.[0]?.url}
-//         alt={song?.name}
-//       />
-//       <div className="flex-1 flex flex-col justify-center mx-3">
-//         {!artistId ? (
-//           <Link to={`/songs/${song?.id}`} onClick={(e) => e.stopPropagation()}>
-//             <p className="text-xl font-bold text-white">{song?.name}</p>
-//           </Link>
-//         ) : (
-//           <p className="text-xl font-bold text-white">{song?.name}</p>
-//         )}
-//         <p className="text-base text-gray-300 mt-1">
-//           {song?.artists?.[0]?.name || "Unknown Artist"}
-//         </p>
-//       </div>
-//     </div>
-//     {!artistId ? (
-//       <PlayPause
-//         isPlaying={isPlaying}
-//         activeSong={activeSong}
-//         song={song}
-//         handlePause={handlePauseClick}
-//         handlePlay={() => handlePlayClick(song, i)}
-//       />
-//     ) : null}
-//   </div>
-// );
-
-// export default SongBar;
-
 import { Link } from "react-router-dom";
 import PlayPause from "./PlayPause";
 
@@ -62,8 +12,8 @@ const SongBar = ({
 }) => {
   return (
     <div
-      className={`w-full flex flex-row items-center hover:bg-[#4c426e] ${
-        activeSong?.id === song?.id ? "bg-[#4c426e]" : "bg-transparent"
+      className={`w-full flex flex-row items-center hover:bg-[#4c4c4c] ${
+        activeSong?.id === song?.id ? "bg-[#4c4c4c]" : "bg-transparent"
       } p-2 rounded-lg cursor-pointer mb-2`}
     >
       <div className="flex-1 flex flex-row justify-between items-center">
@@ -73,20 +23,14 @@ const SongBar = ({
           alt={song?.name}
         />
         <div className="flex-1 flex flex-col justify-center mx-3">
-          <Link
-            to={`/songs/${song?.id}`}
-            onClick={(e) => e.stopPropagation()}
-          >
+          <Link to={`/songs/${song?.id}`} onClick={(e) => e.stopPropagation()}>
             <p className="text-xl font-bold text-white">{song?.name}</p>
           </Link>
 
-          {/* <p className="text-base text-gray-300 mt-1">
-            {song?.artists?.[0]?.name || "Unknown Artist"}
-          </p> */}
           <Link to={`/artists/${artistId || "#"}`}>
             <p className="text-base text-gray-300 mt-1">
-            {song?.artists?.[0]?.name || "Unknown Artist"}
-          </p>
+              {song?.artists?.[0]?.name || "Unknown Artist"}
+            </p>
           </Link>
         </div>
       </div>
