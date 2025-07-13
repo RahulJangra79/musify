97d85a47c6f41e5a10b79730e33629a910960b04
Rahul Jangra <rjangra4015@gmail.com>
2025-07-13 17:19:57 +0530

    added followers

--- frontend/src/components/DetailsHeader.jsx
+++ frontend/src/components/DetailsHeader.jsx
@@ -16,12 +16,11 @@ const DetailsHeader = ({ artistId, artistData, songData }) => {
     <div className="relative w-full flex flex-col">
       <div className="w-full bg-gradient-to-l from-transparent to-black sm:h-48 h-28" />
 
-      {/* <div className="absolute inset-0 items-center">
+      <div className="absolute inset-0 flex items-center">
         <img
           alt="profile"
           src={profileImage}
-          // className="sm:w-48 w-28 sm:h-48 h-28 rounded-full object-cover border-2 shadow-xl shadow-black"
-          className="w-full sm:h-48 h-56 object-cover border-2 shadow-xl shadow-black"
+          className="sm:w-48 w-28 sm:h-48 h-28 object-cover border-2 shadow-xl shadow-black"
         />
 
         <div className="ml-5">
@@ -43,39 +42,6 @@ const DetailsHeader = ({ artistId, artistData, songData }) => {
             <p className="text-sm text-gray-300 mt-1">
               Followers:{" "}
               <span className="font-semibold text-white">{followers}</span>
-            </p>
-          )}
-        </div>
-      </div> */}
-
-      <div className="relative w-full flex flex-col">
-        {/* Full-width banner image */}
-        <img
-          alt="profile"
-          src={profileImage}
-          className="w-full h-[50vh] object-cover shadow-xl shadow-black"
-        />
-
-        {/* Info container under image */}
-        <div className="mt-6 px-6">
-          <p className="font-extrabold text-4xl text-white">{displayName}</p>
-
-          {!artistId && (
-            <Link to={`/artists/${songData?.artists?.[0]?.id}`}>
-              <p className="text-lg text-gray-400 mt-2">
-                {songData?.artists?.[0]?.name}
-              </p>
-            </Link>
-          )}
-
-          <p className="text-lg text-gray-400 mt-2">{genreOrYear}</p>
-
-          {artistId && (
-            <p className="text-lg text-gray-300 mt-2">
-              Followers:{" "}
-              <span className="text-white font-semibold text-xl">
-                {followers}
-              </span>
             </p>
           )}
         </div>
